7a01973e743f97960423ac10bf4b85843c4eb870
NoReply Amazon <no-reply@amazon.com>
2025-02-02 06:02:51 +0000

    Revert "[Commit] Iteration 7 #errors = 59 <= 70."
    This reverts commit 750e049908dfd35a43740b91a642446b61384f9b.

--- app/Bookstore.Web/Areas/Admin/Models/Inventory/InventoryCreateUpdateViewModel.cs
+++ app/Bookstore.Web/Areas/Admin/Models/Inventory/InventoryCreateUpdateViewModel.cs
@@ -7,7 +7,6 @@ using Bookstore.Domain.Books;
 using Bookstore.Domain.ReferenceData;
 using Bookstore.Web.Helpers;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 
 namespace Bookstore.Web.Areas.Admin.Models.Inventory
@@ -84,7 +83,7 @@ namespace Bookstore.Web.Areas.Admin.Models.Inventory
         [MaxFileSize(2*1024*1024)]
         [ImageTypes(new string[] {".png", ".jpg", ".jpeg"})]
         [DisplayName("Cover image")]
-        public IFormFile CoverImage { get; set; }
+        public HttpPostedFileBase CoverImage { get; set; }
         
         public string CoverImageUrl { get; set; }
 
