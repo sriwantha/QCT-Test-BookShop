bdd1810ca3c2f2ed536dda9fe4020ff52c27c9a3
NoReply Amazon <no-reply@amazon.com>
2025-02-02 06:19:12 +0000

    [Commit] Iteration 23 #errors = 6 <= 6.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Startup.cs][CS0103][18,13] The name 'LoggingSetup' does not exist in the current context``.

--- app/Bookstore.Web/Startup.cs
+++ app/Bookstore.Web/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Bookstore.Common;
 
 namespace Bookstore.Web
 {
