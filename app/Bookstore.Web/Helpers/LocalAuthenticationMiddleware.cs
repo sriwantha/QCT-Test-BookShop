31456584785bcae48f54334f939fb4836d83bd9d
NoReply Amazon <no-reply@amazon.com>
2025-02-02 22:00:34 +0000

    [Commit] Iteration 11 #errors = 44 <= 44.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Helpers/LocalAuthenticationMiddleware.cs][CS0246][11,50] The type or namespace name 'OwinMiddleware' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Helpers/LocalAuthenticationMiddleware.cs
+++ app/Bookstore.Web/Helpers/LocalAuthenticationMiddleware.cs
@@ -1,25 +1,23 @@
 using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System.Web;
 using Bookstore.Domain.Customers;
-using Microsoft.AspNetCore.Owin;
-
+using Microsoft.AspNetCore.Http;
 
 namespace Bookstore.Web.Helpers
 {
-    public class LocalAuthenticationMiddleware : OwinMiddleware
+    public class LocalAuthenticationMiddleware : IMiddleware
     {
         private const string UserId = "FB6135C7-1464-4A72-B74E-4B63D343DD09";
 
         private readonly ICustomerService _customerService;
 
-        public LocalAuthenticationMiddleware(OwinMiddleware next, ICustomerService customerService) : base(next)
+        public LocalAuthenticationMiddleware(ICustomerService customerService)
         {
             _customerService = customerService;
         }
 
-        public override async Task Invoke(IOwinContext context)
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
             if (context.Request.Path.Value.StartsWith("/Authentication/Login"))
             {
@@ -47,7 +45,7 @@ namespace Bookstore.Web.Helpers
             }
         }
 
-        private void CreateClaimsPrincipal(IOwinContext context)
+        private void CreateClaimsPrincipal(HttpContext context)
         {
             var identity = new ClaimsIdentity("Application");
 
@@ -57,12 +55,12 @@ namespace Bookstore.Web.Helpers
             identity.AddClaim(new Claim("family_name", "User"));
             identity.AddClaim(new Claim(ClaimTypes.Role, "Administrators"));
 
-            context.Request.User = new ClaimsPrincipal(identity);
+            context.User = new ClaimsPrincipal(identity);
         }
 
-        private async Task SaveCustomerDetailsAsync()
+        private async Task SaveCustomerDetailsAsync(HttpContext context)
         {
-            var identity = (ClaimsIdentity)HttpContext.Current.User.Identity;
+            var identity = (ClaimsIdentity)context.User.Identity;
 
             var dto = new CreateOrUpdateCustomerDto(
                 identity.FindFirst("nameidentifier").Value,
