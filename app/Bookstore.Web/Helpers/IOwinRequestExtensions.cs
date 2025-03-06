1fced31c8ee946706753c23f033f306a52051765
NoReply Amazon <no-reply@amazon.com>
2025-03-06 01:22:26 +0000

    [Commit] Iteration 6 #errors = 73 <= 73.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Helpers/IOwinRequestExtensions.cs][CS0246][8,48] The type or namespace name 'IOwinRequest' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Helpers/IOwinRequestExtensions.cs
+++ app/Bookstore.Web/Helpers/IOwinRequestExtensions.cs
@@ -1,12 +1,13 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Owin;
-
 
 namespace Bookstore.Web.Helpers
 {
     public static class OwinRequestExtensions
     {
-        public static string GetReturnUrl(this IOwinRequest request)
+        public static string GetReturnUrl(this IOwinContext context)
         {
+            var request = context.Request;
             return $"{request.Scheme}://{request.Host}/signin-oidc";
         }
     }
