352d92c4765a08afaa4314719f0a2584dd5a2934
NoReply Amazon <no-reply@amazon.com>
2025-03-06 01:40:18 +0000

    [Commit] Iteration 22 #errors = 5 <= 10.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Controllers/AuthenticationController.cs][CS1061][27,46] 'IResponseCookies' does not contain a definition for 'Add' and no accessible extension method 'Add' accepting a first argument of type 'IResponseCookies' could be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Controllers/AuthenticationController.cs
+++ app/Bookstore.Web/Controllers/AuthenticationController.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Web;
 using BobsBookstoreClassic.Data;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Http;
 
 namespace Bookstore.Web.Controllers
 {
@@ -22,9 +21,12 @@ namespace Bookstore.Web.Controllers
 
         private ActionResult LocalSignOut()
         {
-            if (HttpContext.Request.Cookies["LocalAuthentication"] != null)
+            if (HttpContext.Request.Cookies.TryGetValue("LocalAuthentication", out _))
             {
-                HttpContext.Response.Cookies.Add(new HttpCookie("LocalAuthentication") { Expires = DateTime.Now.AddDays(-1) });
+                HttpContext.Response.Cookies.Append("LocalAuthentication", "", new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(-1)
+                });
             }
 
             return RedirectToAction("Index", "Home");
@@ -32,14 +34,17 @@ namespace Bookstore.Web.Controllers
 
         private ActionResult CognitoSignOut()
         {
-            if (Request.Cookies[".AspNet.Cookies"] != null)
+            if (HttpContext.Request.Cookies.TryGetValue(".AspNet.Cookies", out _))
             {
-                Response.Cookies.Add(new HttpCookie(".AspNet.Cookies") { Expires = DateTime.Now.AddDays(-1) });
+                HttpContext.Response.Cookies.Append(".AspNet.Cookies", "", new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(-1)
+                });
             }
 
             var domain = BookstoreConfiguration.Get("Authentication/Cognito/CognitoDomain");
             var clientId = BookstoreConfiguration.Get("Authentication/Cognito/LocalClientId");
-            var logoutUri = $"{Request.Url.Scheme}://{Request.Url.Host}:{Request.Url.Port}/";
+            var logoutUri = $"{Request.Scheme}://{Request.Host}/";
 
             return Redirect($"{domain}/logout?client_id={clientId}&logout_uri={logoutUri}");
         }
